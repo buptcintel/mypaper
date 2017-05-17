57e307fcc8ba56e551b1041043b591e91371998d
xrf416933696 <xrf930301@163.com>
2017-05-17 13:50:29 +0800

    1.实体类

---
+++ src/com/bupt/xrf/entity/ParkVehicle.java
@@ -0,0 +1,54 @@
+package com.bupt.xrf.entity;
+
+public class ParkVehicle {
+	
+	private String pvid;
+	private Vehicle vehicle;
+	private Logpark logpark;
+	private String ifuse;
+	private int vamount;
+	private int useamount;
+	
+	public String getPvid() {
+		return pvid;
+	}
+	public void setPvid(String pvid) {
+		this.pvid = pvid;
+	}
+	public Vehicle getVehicle() {
+		return vehicle;
+	}
+	public void setVehicle(Vehicle vehicle) {
+		this.vehicle = vehicle;
+	}
+	public Logpark getLogpark() {
+		return logpark;
+	}
+	public void setLogpark(Logpark logpark) {
+		this.logpark = logpark;
+	}
+	public String getIfuse() {
+		return ifuse;
+	}
+	public void setIfuse(String ifuse) {
+		this.ifuse = ifuse;
+	}
+	public int getVamount() {
+		return vamount;
+	}
+	public void setVamount(int vamount) {
+		this.vamount = vamount;
+	}
+	public int getUseamount() {
+		return useamount;
+	}
+	public void setUseamount(int useamount) {
+		this.useamount = useamount;
+	}
+	
+	@Override
+	public String toString() {
+		return "ParkVehicle [pvid=" + pvid + ", vehicle=" + vehicle + ", logpark=" + logpark + ", ifuse=" + ifuse
+				+ ", vamount=" + vamount + ", useamount=" + useamount + "]";
+	}
+}
---
+++ src/com/bupt/xrf/entity/Vehicle.java
@@ -0,0 +1,28 @@
+package com.bupt.xrf.entity;
+
+public class Vehicle {
+
+	private String vid;
+	private String v_name;
+	private String v_power;
+	
+	public String getVid() {
+		return vid;
+	}
+	public void setVid(String vid) {
+		this.vid = vid;
+	}
+	public String getV_name() {
+		return v_name;
+	}
+	public void setV_name(String v_name) {
+		this.v_name = v_name;
+	}
+	public String getV_power() {
+		return v_power;
+	}
+	public void setV_power(String v_power) {
+		this.v_power = v_power;
+	}
+	
+}
